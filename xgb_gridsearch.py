8db8ee469007dcbf078bdbd4f229c3f645d176b9
Vihuhol18 <59253859+Vihuhol18@users.noreply.github.com>
2023-03-10 13:55:57 +0300

    Update xgb_gridsearch.py

--- xgb_gridsearch.py
+++ xgb_gridsearch.py
@@ -27,11 +27,8 @@ def clean_text(s): #remove some trash from text
 
 
 
-df_test = pd.read_csv('df_test')
+
 df_train = pd.read_csv('df_train')
-
-
-df_test = shuffle(df_test, random_state=13)
 df_train = shuffle(df_train, random_state=13)
 
 #prepare train
@@ -41,24 +38,12 @@ X_train = X_train.to_numpy()
 X_ltext_train = df_train.loc[:, 'l_text']
 y_train = df_train.loc[:, 'rating'].to_numpy() - 1 #1-10 to 0-9
 y_train[y_train >= 6] -= 2 #6-9 to 4-7
-y_train_bin = df_train.loc[:, 'estimate'].to_numpy()
 
-#prepare test
-X_test = df_test.loc[:, 'text']
-X_test = X_test.apply(lambda x: clean_text(x))
-X_test = X_test.to_numpy()
-y_test = df_test.loc[:, 'rating'].to_numpy() - 1 #1-10 to 0-9
-y_test[y_test >= 6] -= 2 #6-9 to 4-7
-y_test_bin = df_test.loc[:, 'estimate'].to_numpy()
-
-
-print(np.min(y_train), np.max(y_train), np.min(y_test), np.max(y_test))
 
 #word to vec
-vectorizer_tv = TfidfVectorizer(ngram_range=(1,3), min_df=50)
+vectorizer_tv = TfidfVectorizer(ngram_range=(1,3), min_df=50) #selection of ngram with frequency greater than 50
 vectorizer_tv = vectorizer_tv.fit(X_train)
 X_train = vectorizer_tv.transform(X_train)
-X_test = vectorizer_tv.transform(X_test)
 print('shape X_train ', X_train.shape)
 
 
